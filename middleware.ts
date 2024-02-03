9431440ac4b09a39130e6717f482b9db5fbf2c8f
FelipeMDantas <felipemdantas@terra.com.br>
2024-02-02 22:43:17 -0300

    Authentication configuation, middleware and routes coded.

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { apiAuthPrefix } from "./routes";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
