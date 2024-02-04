821df1aa38d37608e9ca38f04f48e9a33cc152c8
FelipeMDantas <felipemdantas@terra.com.br>
2024-02-04 14:32:56 -0300

    Authentication with GitHub and Google established.

--- actions/login.ts
+++ actions/login.ts
@@ -3,6 +3,7 @@
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -20,5 +21,16 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
-  } catch (error) {}
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+
+    throw error;
+  }
 };
--- auth.ts
+++ auth.ts
@@ -1,7 +1,18 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "./data/user";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +20,31 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    //@ts-ignore
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
