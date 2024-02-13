9166215b7fe59971d28fc355ffff8f766335f076
FelipeMDantas <felipemdantas@terra.com.br>
2024-02-13 20:16:57 -0300

    Modal dialog applied to sign in screen and new redirections coded.

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import LoginForm from "./login-form";
 
 interface LoginButtonProps {
   children: React.ReactNode;
@@ -18,6 +20,17 @@ export const LoginButton = ({
   const onClick = () => {
     router.push("/auth/login");
   };
+
+  if (mode === "modal") {
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className="p-0 w-auto bg-transparent border-none">
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
 
   return (
     <span className="cursor-pointer" onClick={onClick}>
